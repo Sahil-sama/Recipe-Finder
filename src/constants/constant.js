94899476cf6a3b5cb0a14571b12742b49460fe25
Sahil-sama <125532441+Sahil-sama@users.noreply.github.com>
2024-01-01 10:02:55 +0530

    Add files via upload

---
+++ src/constants/constant.js
@@ -0,0 +1,2 @@
+export const logo ='https://a.storyblok.com/f/178900/580x337/765abfe57c/c54b1798f9cf90826479d3e9ca149fcd1371448898_full.png/m/580x337';
+export const API_URL ='https://forkify-api.herokuapp.com/api';
